6c72a8723d6d957628b64a2eb50a1836224bd699
manickam-reactjs <d4575659@gmail.com>
2025-12-17 21:29:25 +0530

    Fix: specific path for BookingController

--- Routes/BookingRoute.js
+++ Routes/BookingRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bookingControllerookingController = require("../Controllers/bookingController");
+const bookingController = require("../Controllers/bookingController");
 const authController = require("../Controllers/authController");
 
 const router = express.Router();
