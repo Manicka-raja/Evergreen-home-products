61a8653c9ff5040e828d7c0e73fab8eaf60da7d4
manickam-reactjs <d4575659@gmail.com>
2025-12-18 20:05:42 +0530

    fix multer filename for user and product images

--- schemas/multer.js
+++ schemas/multer.js
@@ -1,14 +1,24 @@
 const multer = require("multer");
+const path = require("path");
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    if (file.fieldname === "photo") cb(null, "public/img/user");
-    else if (file.fieldname === "image") cb(null, "public/img/products");
+    if (file.fieldname === "photo") {
+      cb(null, "public/img/user");
+    } else if (file.fieldname === "image") {
+      cb(null, "public/img/products");
+    } else {
+      cb(new Error("Invalid field name"));
+    }
   },
 
   filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `user-${Date.now()}.${ext}`);
+    const ext = path.extname(file.originalname); // safer
+    if (file.fieldname === "photo") {
+      cb(null, `user-${req.user.id}-${Date.now()}${ext}`);
+    } else if (file.fieldname === "image") {
+      cb(null, `product-${Date.now()}${ext}`);
+    }
   },
 });
 
